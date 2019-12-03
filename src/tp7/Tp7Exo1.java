ae7216523debec590740eca8c9db5876363186ce
Marcial <Marcial@DESKTOP-PE9LBK9>
2019-12-03 22:22:39 +0100

    ok

---
+++ src/tp7/Tp7Exo1.java
@@ -0,0 +1,32 @@
+package tp7;
+
+import java.util.Scanner;
+
+public class Tp7Exo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Saisissez le rayon de votre cercle en cm: ");
+		double rayon = sc.nextDouble();
+		
+		double perimetre = perimetre(rayon);
+		System.out.println("Le perimetre de votre cercle est : "+perimetre+"cm");
+		double surface = surface(rayon);
+		System.out.println("Le perimetre de votre cercle est : "+surface+"cm");
+	}
+
+	public static double perimetre(double rayon) {
+		double resultat;
+		resultat = 2 * Math.PI * rayon;
+		return resultat;
+
+	}
+
+	public static double surface(double rayon) {
+		double resultat;
+		resultat = 4 / 3 * Math.PI * Math.pow(rayon, 3);
+		return resultat;
+	}
+}
---
+++ src/tp7/Tp7Exo2.java
@@ -0,0 +1,32 @@
+package tp7;
+
+import java.util.Scanner;
+
+public class Tp7Exo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[] tab = new int[3];
+
+		for (int i = 0; i < tab.length; i++) {
+			System.out.println("Saisissez un entier " + i + ": ");
+			tab[i] = sc.nextInt();
+		}
+
+		int max = plusGrand(tab);
+
+		System.out.println("le plus grand chiffre est :" + max);
+	}
+
+	public static int plusGrand(int[] tab) {
+		int max = tab[0];
+		for (int i = 0; i < tab.length; i++) {
+			if (tab[i] > max) {
+				max = tab[i];
+			}
+			System.out.println(max);
+		}
+		return max;
+	}
+}
